1088488618aec17af0482cf0d209ad0bb1e4a1eb
Matin Rezaei <matinrco@gmail.com>
2024-05-11 15:39:45 +0330

    Add sample theme customization + types

---
+++ additional.d.ts
@@ -0,0 +1,17 @@
+/**
+ * custom TS type definitions.
+ * here is sample mantine type extension & override.
+ * first check @/lib/hooks/createTheme.ts.
+ */
+// import { DefaultMantineColor, MantineColorsTuple } from "@mantine/core";
+
+// type ExtendedCustomColors = "brand" | DefaultMantineColor;
+
+// declare module "@mantine/core" {
+//     export interface MantineThemeColorsOverride {
+//         colors: Record<ExtendedCustomColors, MantineColorsTuple>;
+//     }
+//     export interface MantineThemeOther {
+//         customProperty: string;
+//     }
+// }
